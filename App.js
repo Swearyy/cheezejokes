fcd3aa038509cf2723681aad17a69dd79de532c9
Swearyy <130965132+Swearyy@users.noreply.github.com>
2023-11-12 00:19:30 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import JokeList from "./JokeList";
+
+
+import React from "react";
+import JokeList from "./JokeList";
+
+const App = () => {
+  return (
+    <div className="App">
+      <JokeList />
+    </div>
+  );
+}
+
+export default App;
